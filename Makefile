33d1ef6184b60de7fc5f1806be582aebba6ee983
Azizbek <azizbek@Azizbeks-MacBook-Pro.local>
2025-03-25 15:58:18 +0500

    not optimized

---
+++ Makefile
@@ -0,0 +1,5 @@
+db-not-optimized: 
+	docker compose -f devops/docker-compse-psql-1.yml up -d
+
+db-optimized: 
+	
---
+++ migrations/database-not-optimized.sql
@@ -0,0 +1,10 @@
+CREATE TABLE places (
+    id SERIAL PRIMARY KEY,         
+    name VARCHAR(255) NOT NULL,      
+    description TEXT,               
+    latitude DOUBLE PRECISION NOT NULL,  
+    longitude DOUBLE PRECISION NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW() ON UPDATE NOW()
+);
+
